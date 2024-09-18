df4f008db7e2166e2ea27e3842ac164d3ef6e262
cssmh <touristmomen@gmail.com>
2024-09-18 19:40:14 +0600

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,33 @@
+import logo from "../assets/logo.webp";
 import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineBars } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdLocationOn } from "react-icons/md";
-import logo from "../assets/logo.webp"
+import { FaArrowRightToBracket } from "react-icons/fa6";
+import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
+  const { user, logOut } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef(null);
   const buttonRef = useRef(null);
 
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
+  // Toggle menu function
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Close menu if clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
@@ -33,8 +47,7 @@ const Navbar = () => {
   }, [isMenuOpen]);
 
   return (
-    <>
-      {/* Top Bar with Contact Info */}
+    <div>
       <div className="bg-gray-200 text-gray-800 px-3 py-2">
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center space-x-4">
@@ -49,8 +62,6 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
-      {/* Main Navbar */}
       <nav className="bg-green-500 text-white px-3 py-1 relative">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-xl font-bold">
@@ -58,18 +69,19 @@ const Navbar = () => {
               <img src={logo} className="w-[34px]" alt="Logo" />
             </Link>
           </div>
-
-          {/* Mobile Menu Button */}
           <button
             ref={buttonRef}
             onClick={toggleMenu}
             className="md:hidden p-2 focus:outline-none"
           >
-            <AiOutlineBars className="h-6 w-6" />
+            {isMenuOpen ? (
+              <MdClose className="h-6 w-6 text-white" />
+            ) : (
+              <AiOutlineBars className="h-6 w-6 text-white" />
+            )}
           </button>
-
-          {/* Desktop Menu */}
-          <div className="hidden md:flex space-x-4">
+          <div>
+            <div className="hidden md:flex space-x-4">
             <Link to="/" className="block py-2">
               Home
             </Link>
@@ -80,41 +92,62 @@ const Navbar = () => {
               Admin Panel
             </Link>
           </div>
-
-          {/* Mobile Menu */}
+          {user?.email ? (
+            <div>
+              <label
+                tabIndex={0}
+                className="cursor-pointer flex items-center"
+                // onClick={() => setUserDropdownVisible((prev) => !prev)}
+              >
+                <img
+                  src={user.photoURL}
+                  className="w-10 rounded-full hover:shadow-lg transition-shadow duration-300 ease-in-out"
+                  alt="ava"
+                />
+              </label>
+              <button
+                className="text-base hover:bg-gray-100 font-semibold flex justify-center items-center px-[10px] py-1 rounded-full gap-1 transform active:translate-y-0.5 transition-transform duration-150 ease-in-out"
+                onClick={handleLogout}
+              >
+                <FaArrowRightToBracket />
+                <span>Log Out</span>
+              </button>
+            </div>
+          ) : (
+            <Link
+              to="/login"
+              className="btn btn-sm border border-green-400 hover:border-green-400 hover:bg-green-400 hover:text-white"
+            >
+              Login
+            </Link>
+          )}
+          </div>
           <div
             ref={menuRef}
-            className={`fixed top-0 left-0 h-full w-2/4 bg-gray-500 z-50 transform ${
+            className={`fixed top-0 left-0 h-full w-2/5 bg-green-700 text-white z-50 transition-transform transform ${
               isMenuOpen ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-300 ease-in-out md:hidden`}
+            } duration-300 ease-in-out md:hidden`}
           >
-            <div className="flex flex-col space-y-2 p-4">
-              <Link
-                to="/"
-                className="block py-2"
-                onClick={() => setIsMenuOpen(false)}
-              >
+            <div className="flex justify-end p-4">
+              <button onClick={toggleMenu} className="text-white">
+                <MdClose className="h-6 w-6" />
+              </button>
+            </div>
+            <div className="flex flex-col space-y-4 p-4">
+              <Link to="/" className="block py-2" onClick={toggleMenu}>
                 Home
               </Link>
-              <Link
-                to="/add-order"
-                className="block py-2"
-                onClick={() => setIsMenuOpen(false)}
-              >
+              <Link to="/add-order" className="block py-2" onClick={toggleMenu}>
                 Add Sales Order
               </Link>
-              <Link
-                to="/admin"
-                className="block py-2"
-                onClick={() => setIsMenuOpen(false)}
-              >
+              <Link to="/admin" className="block py-2" onClick={toggleMenu}>
                 Admin Panel
               </Link>
             </div>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
